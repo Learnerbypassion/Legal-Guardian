8d68096c6f0cc7079136d3e6265ab12afd7cea47
Learnerbypassion <callmedad002022@gmail.com>
2026-05-11 00:03:04 +0530

    Fixed link

--- frontend/src/constants/links.js
+++ frontend/src/constants/links.js
@@ -1,5 +1,5 @@
 export const LINKS = {
-  WEBSITE: 'https://github.com/AnkanDey05/Legal-Guardian',
+  WEBSITE: 'https://github.com/Learnerbypassion/Legal-Guardian',
   APP_REPO: 'https://expo.dev/accounts/sohamdev/projects/mobile/builds/c196e5c7-cef6-4ed7-82c4-14aa4770205e',
   EXTENSION_REPO: 'https://github.com/AnkanDey05/Legal-Guardian-extension'
 };
